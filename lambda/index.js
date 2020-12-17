886eefc8d568520db9b5079509a8dd83175c27ab
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-12-17 13:15:32 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -38,6 +38,8 @@ async function addDefaultPlz(handlerInput, entry){
     let defaultPlzs = attributes.default_plzs;
     defaultPlzs.append(entry);
     
+    console.log(attributes);
+    
     attributesManager.setPersistentAttributes(attributes);
     await attributesManager.savePersistentAttributes();
 }
