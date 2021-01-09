550e141ce2c8ede15cd937430a3665986f2ce2a4
Sebastian Winkler <sebastian.winkler@students.fh-hagenberg.at>
2021-01-09 17:27:45 +0100

    deleted polluting json object

--- lambda/index.js
+++ lambda/index.js
@@ -329,7 +329,7 @@ const GetCasesIntentHandler = {
         if(plz !== 0){
             let result = await axios.get('https://mpg9pvi8j0.execute-api.us-east-1.amazonaws.com/cases/' + plz);
             let warnstufe = getWarnstufenColor(result.data.Warnstufe);
-            {"bezirk":"Linz-Land","anzahlEinwohner":"151371","anzahl":"6581","anzahlTot":"73","anzahl7Tage":"203"}
+
             speakOutput = "Im Bezirk " + result.data.bezirk + " hat es bisher " + result.data.anzahl + 
             " Fälle gegeben." + " Insgesamt sind " + result.data.anzahlTot + " an Covid19 verstorben." +
             " In den letzten 7 Tagen hat es " + result.data.anzahl7Tage + " Neuinfektionen in diesem Bezirk gegeben.";
